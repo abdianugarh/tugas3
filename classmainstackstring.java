28bb6e9345fd8b7cb859d5d08f93df791bf1aa48
abdianugarh <43585419+abdianugarh@users.noreply.github.com>
2018-10-31 20:10:05 +0700

    Create classmainstackstring.java

---
+++ classmainstackstring.java
@@ -0,0 +1,20 @@
+public class classmainstackstring {
+
+	
+	public static void main(String[] args) {
+		classstackstring st= new classstackstring();
+		
+		
+		st.push("qadri");st.push("fandhy");
+		st.push("ucup");
+		st.push("masdar");
+		st.push("diank");
+		
+		String d0=st.pop();
+		System.out.println(d0);
+		st.cetak();
+
+
+	}
+
+}
