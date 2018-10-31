b2c6f769b42ca16b1fa3389dc6293aed3255b209
abdianugarh <43585419+abdianugarh@users.noreply.github.com>
2018-10-31 20:16:53 +0700

    Create classmainqueuestring.java

---
+++ classmainqueuestring.java
@@ -0,0 +1,16 @@
+public class classmainqueuestring {
+
+	
+	public static void main(String[] args) {
+		classqueuestring q=new classqueuestring();
+		q.cetak();
+		q.insert("ALIEF");
+		q.cetak();
+		q.insert("ABDI");
+		q.cetak();
+		q.insert("QADRI");
+		q.cetak();
+
+	}
+
+}
