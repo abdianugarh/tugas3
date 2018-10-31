d634deb3d4dff2f0b845e0669aac7f698b7ec0f6
abdianugarh <43585419+abdianugarh@users.noreply.github.com>
2018-10-31 21:36:04 +0700

    Create classmainqueuegeneric.java

---
+++ classmainqueuegeneric.java
@@ -0,0 +1,28 @@
+public class classmainqueuegeneric {
+
+	
+	public static void main(String[] args) {
+
+
+
+		classqueuegeneric <Integer> sc = new classqueuegeneric <Integer>();
+
+			    sc.insert(1);
+
+			    sc.insert(2);
+
+			    sc.insert(3);
+
+			    
+
+			    System.out.print("Hasil :");
+
+			    System.out.println(sc.remove());
+
+			 
+
+			}
+
+	
+
+}
